ad768b4c51be0d1fa578463d151dea47ec5070ee
Joni Oliveira <joni.oliveira89@gmail.com>
2021-01-04 21:01:12 +0000

    chore: add final tests and tweaks (#5)

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -15,11 +15,11 @@ func FilesAreEqual(file1, file2 []byte) (bool, error) {
 	var err error
 	err = json.Unmarshal(file1, &result1)
 	if err != nil {
-		return false, fmt.Errorf("Error mashalling string 1 :: %s", err.Error())
+		return false, fmt.Errorf("Error mashalling file 1: %s", err.Error())
 	}
 	err = json.Unmarshal(file2, &result2)
 	if err != nil {
-		return false, fmt.Errorf("Error mashalling string 2 :: %s", err.Error())
+		return false, fmt.Errorf("Error mashalling file 2: %s", err.Error())
 	}
 
 	return equality.JsonObjectsEquals(result1, result2), nil
@@ -38,7 +38,7 @@ func ReadFiles(paths []string) (map[string][]byte, error) {
 		byteValue, _ := ioutil.ReadAll(jsonFile)
 
 		if !json.Valid(byteValue) {
-			return nil, fmt.Errorf("Invalid json file with path %s", path)
+			return filesContentMap, fmt.Errorf("Invalid json file with path %s", path)
 		}
 
 		filesContentMap[path] = byteValue
--- pkg/validator/validator_test.go
+++ pkg/validator/validator_test.go
@@ -8,32 +8,42 @@ import (
 
 func TestValidatorArguments(t *testing.T) {
 	t.Run("No args", func(t *testing.T) {
-		assert.Nil(t, nil)
+		ok := ValidateArgumentsSize(0)
+		assert.Equal(t, false, ok)
 	})
 
 	t.Run("Less then needed args", func(t *testing.T) {
-		assert.Nil(t, nil)
+		ok := ValidateArgumentsSize(1)
+		assert.Equal(t, false, ok)
 	})
 
 	t.Run("Right number of args", func(t *testing.T) {
-		assert.Nil(t, nil)
+		ok := ValidateArgumentsSize(2)
+		assert.Equal(t, true, ok)
 	})
 
 	t.Run("More than needed Args", func(t *testing.T) {
-		assert.Nil(t, nil)
+		ok := ValidateArgumentsSize(3)
+		assert.Equal(t, false, ok)
 	})
 }
 
 func TestValidatorFilePath(t *testing.T) {
 	t.Run("Valid file path", func(t *testing.T) {
-		assert.Nil(t, nil)
+		ok, path := ValidateFilesPath([]string{"../../assets/file1.json"})
+		assert.Equal(t, true, ok)
+		assert.Equal(t, "", path)
 	})
 
 	t.Run("Invalid file path", func(t *testing.T) {
-		assert.Nil(t, nil)
+		ok, path := ValidateFilesPath([]string{"../../assets/notExists.json"})
+		assert.Equal(t, false, ok)
+		assert.Equal(t, "../../assets/notExists.json", path)
 	})
 
 	t.Run("One valid and one invalid file path", func(t *testing.T) {
-		assert.Nil(t, nil)
+		ok, path := ValidateFilesPath([]string{"../../assets/file1.json", "../../assets/notExists.json"})
+		assert.Equal(t, false, ok)
+		assert.Equal(t, path, "../../assets/notExists.json")
 	})
 }
